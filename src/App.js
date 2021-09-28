c954c05c72c4315d53a55eeb5a8deca8a32e0439
yefalek <yefremenkov.alek@gmail.com>
2021-09-28 11:40:58 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Form from "./components/ContactForm/Form";
 import ContactsList from "./components/ContactsList/ContsctsList";
+import Filter from "./components/Filter/Filter";
 import "./App.css";
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
   };
 
   handleAddContact = (newContact) =>
@@ -28,8 +34,16 @@ class App extends Component {
       contacts: contacts.filter((contact) => contact.id !== id),
     }));
 
+  handleFilterChange = (filter) => this.setState({ filter });
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter((contact) => contact.name.includes(filter));
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
     return (
       <>
         <h2>Form Contact</h2>
@@ -37,7 +51,12 @@ class App extends Component {
           onAdd={this.handleAddContact}
           onCheckUnique={this.handleCheckUniqueContact}
         />
-        <ContactsList contacts={contacts} onRemove={this.handleRemoveContact} />
+        <h2>Contacts list</h2>
+        <Filter filter={filter} onChange={this.handleFilterChange} />
+        <ContactsList
+          contacts={visibleContacts}
+          onRemove={this.handleRemoveContact}
+        />
       </>
     );
   }
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,12 @@
+const Filter = ({ filter, onChange }) => {
+  return (
+    <input
+      type="text"
+      name="filter"
+      value={filter}
+      onChange={({ target }) => onChange(target.value)}
+      placeholder="Search..."
+    />
+  );
+};
+export default Filter;
